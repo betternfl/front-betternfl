ad8a629e2b6cd90e6aea3fdfcfc277c67cacd4a3
KaiqueYamane <kaiquemaney@hotmail.com>
2018-04-17 11:43:08 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { LoginPageModule } from '../pages/login/login.module';
+import { SingupPageModule } from '../pages/singup/singup.module';
+import { SinginwithemailPageModule } from '../pages/singinwithemail/singinwithemail.module';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    LoginPageModule,
+    SingupPageModule,
+    SinginwithemailPageModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/singinwithemail/singinwithemail.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SinginwithemailPage } from './singinwithemail';
+
+@NgModule({
+  declarations: [
+    SinginwithemailPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SinginwithemailPage),
+  ],
+})
+export class SinginwithemailPageModule {}
