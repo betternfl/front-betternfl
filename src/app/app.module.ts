755a70445a8ef4d0ad2d6f6cfee4a7c081ef4ceb
Unknown <gabrielpdias@hotmail.com>
2018-04-22 12:15:12 -0300

    accordion-list
    Adicionando a expandable list na tela home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,16 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoginPageModule } from '../pages/login/login.module';
 import { SingupPageModule } from '../pages/singup/singup.module';
 import { SinginwithemailPageModule } from '../pages/singinwithemail/singinwithemail.module';
+import { ExpandableComponent } from '../components/expandable/expandable';
 
 @NgModule({
   declarations: [
@@ -21,6 +20,7 @@ import { SinginwithemailPageModule } from '../pages/singinwithemail/singinwithem
     ContactPage,
     HomePage,
     TabsPage,
+    ExpandableComponent,
   ],
   imports: [
     BrowserModule,
