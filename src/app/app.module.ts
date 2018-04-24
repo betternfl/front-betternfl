f87323122c8c67ebd2a6dc8ee30643fdad087e39
KaiqueYamane <kaiquemaney@hotmail.com>
2018-04-24 11:28:26 -0300

    Carregando ExpandableComponent SingupPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -17,7 +16,6 @@ import { ComponentsModule } from '../components/components.module';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
   ],
@@ -33,7 +31,6 @@ import { ComponentsModule } from '../components/components.module';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
--- src/pages/singup/singup.ts
+++ src/pages/singup/singup.ts
@@ -12,21 +12,201 @@ export class SingupPage {
   private email: string;
   private password: string;
   times: any = [];
+  fav = false;
   constructor(public navCtrl: NavController) {
     //O JSON DE TIMES VAI VIR DESSE JEITO ATÉ VOCE PEDIR QUE SEJA DIFERENTE
     this.times = [
       {
-        nomeTime: "Seattle Seahawks",
-        nomeEstadio: "CenturyLink Field",
-        favorito: true,
+        nomeTime: "Arizona Cardinals",
+        favorito: false,
         expanded: false,
-        foto: "assets/imgs/logos/seattleSeahawks.png",
-      }, {
+        foto: "assets/imgs/logos/arizonaCardinals.png",
+      },
+      {
+        nomeTime: "Atlanta Falcons",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/atlantaFalcons.png",
+      },
+      {
+        nomeTime: "Baltimore Ravens",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/baltimoreRavens.png",
+      },
+      {
+        nomeTime: "Buffalo Bills",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/buffaloBills.png",
+      },
+      {
+        nomeTime: "Carolina Panthers",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/carolinaPanters.png",
+      },
+      {
+        nomeTime: "Chicago Bears",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/chicagoBears.png",
+      },
+      {
+        nomeTime: "Cincinnati Bengals",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/cincinatiBengals.png",
+      },
+      {
+        nomeTime: "Cleveland Browns",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/cleverlandBrowns.png",
+      },
+      {
+        nomeTime: "Dallas Cowboys",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/dallasCowboys.png",
+      },
+      {
+        nomeTime: "Denver Broncos",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/denverBroncos.png",
+      },
+      {
+        nomeTime: "Detroit Lions",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/detroitLions.png",
+      },
+      {
+        nomeTime: "Green Bay Packers",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/greenBayPackers.png",
+      },
+      {
+        nomeTime: "Houston Texans",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/houstonTexans.png",
+      },
+      {
+        nomeTime: "Indianapolis Colts",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/indianapolisColts.png",
+      },
+      {
+        nomeTime: "Jacksonville Jaguars",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/jacksonvilleJaguars.png",
+      },
+      {
+        nomeTime: "Kansas City Chiefs",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/kansasCityChiefs.png",
+      },
+      {
+        nomeTime: "Los Angeles Chargers",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/losAngelesChargers.png",
+      },
+      {
+        nomeTime: "Los Angeles Rams",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/losAngelesRams.png",
+      },
+      {
+        nomeTime: "Miami Dolphins",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/miamiDolphins.png",
+      },
+      {
+        nomeTime: "Minnesota Vikings",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/minessotaVikings.png",
+      },
+      {
+        nomeTime: "New England Patriots",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/newEnglandPatriots.png",
+      },
+      {
+        nomeTime: "New Orleans Saints",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/newOrleansSaints.png",
+      },
+      {
+        nomeTime: "New York Giants",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/newYorkGiants.png",
+      },
+      {
+        nomeTime: "New York Jets",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/newYorkJets.png",
+      },
+      {
+        nomeTime: "Oakland Raiders",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/oaklandRaiders.png",
+      },
+      {
+        nomeTime: "Philadelphia Eagles",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/philadelphiaEagles.png",
+      },
+      {
+        nomeTime: "Pittsburgh Steelers",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/pittsburgSteelers.png",
+      },
+      {
         nomeTime: "San Francisco 49ers",
-        nomeEstadio: "Levi's Stadium",
         favorito: false,
         expanded: false,
         foto: "assets/imgs/logos/sanFransisco49ers.png",
+      },
+      {
+        nomeTime: "Seattle Seahawks",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/seattleSeahawks.png",
+      },
+      {
+        nomeTime: "Tampa Bay Buccaneers",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/tampaBayBuccaneers.png",
+      },
+      {
+        nomeTime: "Tennessee Titans",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/tennesseeTitans.png",
+      },
+      {
+        nomeTime: "Washington Redskins",
+        favorito: false,
+        expanded: false,
+        foto: "assets/imgs/logos/washingtonRedskins.png",
       },
     ];
   }
@@ -39,6 +219,13 @@ export class SingupPage {
       }
       return listItem;
     });
+  }
+
+  expandFav() {
+    if (this.fav == false) {
+      this.fav = true
+    } else
+      this.fav = false
   }
 
   favoritaTime(time) {
